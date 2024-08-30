acb4ac9cf769f11c2d402604c95b993d3a37d5d7
SALEHIN <msionlinekingdom@gmail.com>
2024-08-30 21:36:03 +0600

    server link updated

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React from 'react';
 
 const axiosPublic=axios.create({
-    baseURL:'http://localhost:5001'
+    baseURL:'https://professional-network-server.onrender.com/'
 })
 const useAxiosPublic = () => {
     return axiosPublic;
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../Providers/AuthProviders";
 
 const axiosSecure=axios.create({
-    baseURL:'http://localhost:5001'
+    baseURL:'https://professional-network-server.onrender.com/'
 });
 const useAxiosSecure = () => {
     const navigate=useNavigate();
